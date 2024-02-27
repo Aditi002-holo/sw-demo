9d5a1dfbd4ddd6d9af40e1dc132276c9bbb225c6
Aditi <aditideshpande2908@gmail.com>
2024-02-27 16:44:02 +0530

    Cache the files & make it work in offline mode

--- sw-cached-pages.js
+++ sw-cached-pages.js
@@ -1,18 +1,59 @@
+const cacheName = 'v1'
 
+const cacheAssets = [
+    'index.html',
+    'styles.css',
+    'app.js',
+]
 
 
 // Install
 self.addEventListener('install', e => {
     console.log('SW: installed')
+
+    e.waitUntil(
+        caches
+            .open(cacheName)
+            .then(cache => {
+                console.log(cache)
+                console.log('SW: Caching Files')
+                cache.addAll(cacheAssets)
+            })
+            .then(() => self.skipWaiting())
+    )
 })
 
 
 // Activate
 self.addEventListener('activate', e => {
     console.log('SW: activated')
+    // remove unwanted cache
+    // caches is from Cache Storage API
+    e.waitUntil(
+        caches
+            .keys()
+            .then(cacheNames => {
+                return Promise.all(
+                    cacheNames.map(cache => {
+                        if(cache !== cacheName) {
+                            console.log('SW: Clearing Old Cache')
+                            return caches.delete(cache)
+                        }
+                    })
+                )
+            })
+    )
+
 })
 
 // Fetch
+self.addEventListener('fetch', e => {
+    console.log('SW: Fetching')
 
+    e.respondWith(
+        fetch(e.request)
+            .catch(() => caches.match(e.request))
+    )
+})
 
 // Push Notification
