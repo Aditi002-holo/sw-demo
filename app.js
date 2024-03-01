44ae5dbecf8f11d06088cb9b2119bfdfc35888b1
Aditi <aditideshpande2908@gmail.com>
2024-03-01 11:27:51 +0530

    Notification on count change

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 const counter = document.getElementById('counter')
+const container = document.querySelector('.container')
 let currentNumber
 
 counter.addEventListener('click', () => {
-    console.log(counter.textContent)
     currentNumber = counter.textContent
     counter.textContent = parseInt(currentNumber) + 1
+
+    // Notification on count change
+    if(parseInt(counter.textContent) === 10) {
+        notification(10)
+    } else if(parseInt(counter.textContent) === 20) {
+        notification(20)
+    }
 })
+
+// Notification function - Show & Close the notification
+function notification(count) {
+    Notification
+    .requestPermission()
+    .then(permission => {
+        if(permission === 'granted') {
+            const noti = new Notification(`${count}`, {
+                body: `Count is ${count} now`
+            })
+
+            setTimeout(() => {
+                noti.close()
+            }, 3000);
+        }
+    })
+}
+
+
 
 // Register the cache
 if('serviceWorker' in navigator) {
@@ -18,7 +44,9 @@ if('serviceWorker' in navigator) {
     })
 }
 
-// Notification with button
+
+
+// Notification with a button trigger
 
 const notifBtn = document.getElementById('notif-btn')
 
